b07b17e2ac2d87f99135fdf498ae8879a81c2319
Ricardo Rodriguez <rvrodriguez2000@gmail.com>
2025-04-03 15:08:16 -0700

    created the home, contact, and login page. Set up react-router

--- websitePackage/src/App.tsx
+++ websitePackage/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Contact from "./pages/Contact.js";
+import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <nav className="navigation-bar">
+        <ul>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ websitePackage/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+//import "./Contact.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-page">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
