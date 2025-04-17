587880fb0c8b82cd1b525ed568e8b666a4790518
Ricardo Rodriguez <rvrodriguez2000@gmail.com>
2025-04-17 16:59:36 -0700

    Added Logo

--- websitePackage/src/pages/Home.tsx
+++ websitePackage/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 //import "./Contact.css";
+import Logo from "../assets/StayBnb.png";
 
 const Home: React.FC = () => {
   return (
     <div className="home-page">
+      <img src={Logo} alt="Logo" />
       <h1>Home</h1>
     </div>
   );
