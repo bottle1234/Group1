b00d959a23877d7692983c3a82e89c9c11ae966d
22mbach <143040121+22mbach@users.noreply.github.com>
2025-05-03 20:34:14 -0700

    Delete routes directory

--- routes/recommend.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-recommend_bp = Blueprint("recommend", __name__, url_prefix="/recommendations")
-
-@recommend_bp.route("/", methods=["GET"])
-def get_recommendations():
-    return jsonify({"recommendations": ["Modern Loft", "Seaside Villa"]})
