4575466a720837ac2f1e1aedfd506e40c637842d
22mbach <143040121+22mbach@users.noreply.github.com>
2025-05-03 20:35:22 -0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DEBUG = True
-SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///staybnb.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
